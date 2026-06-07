fd06c41fb1e3ec49378b394cfa7878171d48d57b
Zelalem <zelalemazmera1221@gmail.com>
2026-06-07 03:50:19 +0300

    chore: setup Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Analytics } from "@vercel/analytics/react"
 import App from './App.jsx'
 import '../css/style.css'
 import { AuthProvider } from './AuthContext'
@@ -8,6 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
       <App />
+      <Analytics />
     </AuthProvider>
   </React.StrictMode>,
 )
