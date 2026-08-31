f6e8ede042c81857efe0bbf62917cce2e1fcedcd
Zelalem <zelalemazmera1221@gmail.com>
2026-09-01 02:22:58 +0300

    fix(mobile): simplify metro.config.js - remove invalid monorepo root path ref

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,19 +1,5 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
 
 const config = getDefaultConfig(__dirname);
-
-// Ensure abort-controller resolves correctly from the monorepo root node_modules
-config.resolver = config.resolver || {};
-config.resolver.extraNodeModules = {
-  ...config.resolver.extraNodeModules,
-  'abort-controller': path.resolve(__dirname, 'node_modules/abort-controller'),
-};
-
-// Watch root node_modules as well (monorepo support)
-config.watchFolders = [
-  ...(config.watchFolders || []),
-  path.resolve(__dirname, '../../node_modules'),
-];
 
 module.exports = config;
